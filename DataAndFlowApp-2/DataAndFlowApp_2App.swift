539a794eb2f40849a98dab19c6e702cfc09b79c8
Alexey Barinov <begoodbebad2012@gmail.com>
2021-06-09 17:13:52 +0300

    Initial Commit

---
+++ DataAndFlowApp-2/DataAndFlowApp_2App.swift
@@ -0,0 +1,17 @@
+//
+//  DataAndFlowApp_2App.swift
+//  DataAndFlowApp-2
+//
+//  Created by Mac on 09.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct DataAndFlowApp_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
